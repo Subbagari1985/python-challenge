97ed52dbc0179c8aa0990d324e5141a3156f0872
Swapna S <subbagari.swapna@gmail.com>
2021-07-04 15:54:00 -0400

    Py Poll is complete with Inline comments and writing to output text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,62 +2,62 @@ import os
 import csv
 import pandas as pd
 
+#Path to the csv file
 datapath = os.path.join('Resources','election_data.csv')
 
 #Opening the file
 with open (datapath) as election_data_file:
-    #Moving to the next line since there are headers in the csv provided
-    #next(election_data_file)
+    
     #Reading from the csv file
     csv_reader = csv.DictReader(election_data_file, delimiter=",")
-    count = 0
-    Candidate = []
+    
     #Assigning the list from the csv to the variable data
     data = list(csv_reader)
 #Calculating total number of Votes in the csv provided
     Total_Votes = len(data)
-    print("Election Results")
-    print("----------------------------------")
-    print("Total Votes:  " + str(Total_Votes))
-    print("----------------------------------")
-   
+       
  # read specific columns of csv file using Pandas
 df = pd.read_csv(datapath, usecols = ['Voter ID','County','Candidate'])
+
+#List of Candidates participated in the election
 unique_Candidates = df['Candidate'].unique()
-#print(unique_Candidates[1])
-#print("-------------------------------------------------")
+
+#Calculating Number of Votes for each candidate in the election
 Number_Votes_Candidates = pd.value_counts(df['Candidate'].values)
-#print(Number_Votes_Candidates[1])
-#print("-------------------------------------------------")
+
+#Calculating Percentages of Votes received for each candidate.
 for each in unique_Candidates:
     percentages= (Number_Votes_Candidates/Total_Votes)*100
-#print(percentages[1])
-print(unique_Candidates[0] +":   "+str(round(percentages[0],4))+"%    ("+str(Number_Votes_Candidates[0])+")")
-print(unique_Candidates[1] +":   "+str(round(percentages[1],4))+"%    ("+str(Number_Votes_Candidates[1])+")")
-print(unique_Candidates[2] +":   "+str(round(percentages[2],4))+"%    ("+str(Number_Votes_Candidates[2])+")")
-print(unique_Candidates[3] +":   "+str(round(percentages[3],4))+"%    ("+str(Number_Votes_Candidates[3])+")")
 
+#Creating a DataFrame to access information
 result={"Percentages":round(percentages,4),"Votes":Number_Votes_Candidates, "Candidate":unique_Candidates}
 result_df=pd.DataFrame(result)
 result_df = result_df.set_index(round(percentages,4))
-#print(result_df)
+
+#Calculating who won the election using Loc and Dataframe
 Winner=round(percentages.max(), 4)
-#print(Winner)
 winner_name= result_df.loc[Winner,"Candidate"]
-print("----------------------------------")
-print("Winner:  "+winner_name)
-print("----------------------------------")
 
-
-
-#dict={'Candidate':'List_Candidates[0]','Votes':'List_Candidates[1]'}
-#result_df=pd.DataFrame('Candidate':'List_Candidates[0]','Votes':'List_Candidates[1]')
-#print(result_df)
+#Writing Output to text file
+with open("Output.txt", 'x') as f:
     
-#Calculating the total number of
-    #FOr each row in the list "data", convering values into Integer and adding using Sum function.
-   
-  
+    f.write("Election Results"+'\n')
+    f.write("----------------------------------"+'\n')
+    f.write("Total Votes:  " + str(Total_Votes)+'\n')
+    f.write("----------------------------------"+'\n')
+
+    f.write(unique_Candidates[0] +":   "+str(round(percentages[0],4))+"%    ("+str(Number_Votes_Candidates[0])+")"+'\n')
+    f.write(unique_Candidates[1] +":   "+str(round(percentages[1],4))+"%    ("+str(Number_Votes_Candidates[1])+")"+'\n')
+    f.write(unique_Candidates[2] +":   "+str(round(percentages[2],4))+"%    ("+str(Number_Votes_Candidates[2])+")"+'\n')
+    f.write(unique_Candidates[3] +":   "+str(round(percentages[3],4))+"%    ("+str(Number_Votes_Candidates[3])+")"+'\n')
+    f.write("----------------------------------"+'\n')
+    f.write("Winner:  "+winner_name+'\n')
+    f.write("----------------------------------"+'\n')  
+f.close()
+
+#Reading Output from text file
+with open("Output.txt", "r") as f:
+	print(f.read())
   
 
 
